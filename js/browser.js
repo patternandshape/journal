a7048e1e091d9bfa25e8a9c0cf0b25c77277a3a6
Molly Curtin and sara wagner <patternandshape@gmail.com>
2016-03-14 11:44:07 -0700

    add a letter count to journal entry entered

--- js/browser.js
+++ js/browser.js
@@ -1,3 +1,44 @@
+// exports.
+
+function Post(title, post) {
+  // this.date = date;
+  this.title = title;
+  this.post = post;
+}
+
+// function Date(month, day, year){
+//   this.month = month;
+//   this.day = day;
+//   this.year = year;
+// }
+
+// Date.prototype.fullDate = function(){
+//   return this.month + " " + this.day + ", " + this.year;
+// }
+
+Post.prototype.letterCount = function(){
+  return this.post.length;
+}
+
+
+
 $(document).ready(function(){
+  $("form#new-post").submit(function(event){
+
+    // var todayDate = $("#new-date").val();
+    var postTitle = $("#new-post-title").val();
+    var postEntry = $("#new-post-entry").val();
+    var newPost = new Post(postTitle, postEntry);
+    var count = newPost.letterCount();
+
+    $("#show-entry").show();
+    $(".title").text(postTitle);
+    $(".entry").text(postEntry);
+    $(".numberLength").text(count);
+
+    event.preventDefault();
+
+  });
+
 
 });
--- js/journal.js
+++ js/journal.js
@@ -1,1 +1,21 @@
 // exports.
+
+function Post(title, post) {
+  // this.date = date;
+  this.title = title;
+  this.post = post;
+}
+
+// function Date(month, day, year){
+//   this.month = month;
+//   this.day = day;
+//   this.year = year;
+// }
+
+// Date.prototype.fullDate = function(){
+//   return this.month + " " + this.day + ", " + this.year;
+// }
+
+Post.prototype.letterCount = function(){
+  return this.post.length;
+}
