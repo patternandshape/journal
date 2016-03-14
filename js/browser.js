cd31a62f4f018fe997a0040ec9cffe3fb486fd09
Molly Curtin and sara wagner <patternandshape@gmail.com>
2016-03-14 11:53:53 -0700

    add some gulp and browserify

--- js/browser.js
+++ js/browser.js
@@ -1,26 +1,4 @@
-// exports.
-
-function Post(title, post) {
-  // this.date = date;
-  this.title = title;
-  this.post = post;
-}
-
-// function Date(month, day, year){
-//   this.month = month;
-//   this.day = day;
-//   this.year = year;
-// }
-
-// Date.prototype.fullDate = function(){
-//   return this.month + " " + this.day + ", " + this.year;
-// }
-
-Post.prototype.letterCount = function(){
-  return this.post.length;
-}
-
-
+var Post = require('./../js/journal.js').Post;
 
 $(document).ready(function(){
   $("form#new-post").submit(function(event){
